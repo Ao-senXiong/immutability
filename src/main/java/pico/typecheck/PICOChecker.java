130c7f5275c4bc0aea52dc005f166988dc0562c2
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2024-06-27 19:32:22 -0400

    Remove FBC print error for clean up

--- src/main/java/pico/typecheck/PICOChecker.java
+++ src/main/java/pico/typecheck/PICOChecker.java
@@ -2,12 +2,7 @@ package pico.typecheck;
 
 import org.checkerframework.checker.initialization.InitializationChecker;
 import org.checkerframework.common.basetype.BaseTypeChecker;
-import org.checkerframework.framework.source.SupportedOptions;
 
-import java.util.Map.Entry;
-import java.util.Set;
-
-@SupportedOptions({"printFbcErrors"})
 public class PICOChecker extends InitializationChecker {
 
     public PICOChecker() {}
@@ -30,28 +25,11 @@ public class PICOChecker extends InitializationChecker {
 
     @Override
     protected boolean shouldAddShutdownHook() {
-        return hasOption("printFbcErrors") || super.shouldAddShutdownHook();
+        return super.shouldAddShutdownHook();
     }
 
     @Override
     protected void shutdownHook() {
         super.shutdownHook();
-        if (hasOption("printFbcErrors")) {
-            printFbcViolatedMethods();
-        }
-    }
-
-    private void printFbcViolatedMethods() {
-        Set<Entry<String, Integer>> entries = ((PICONoInitVisitor) visitor).fbcViolatedMethods.entrySet();
-        if (entries.isEmpty()) {
-            System.out.println("\n=============== Congrats! No Fbc Violations Found. ===============\n");
-        } else {
-            System.out.println("\n===================== Fbc Violations Found! ======================");
-            System.out.format("%30s%30s\n", "Method", "Violated Times");
-            for (Entry<String, Integer> e : entries) {
-                System.out.format("%30s%30s\n", e.getKey(), e.getValue());
-            }
-            System.out.println("====================================================================\n");
-        }
     }
 }
