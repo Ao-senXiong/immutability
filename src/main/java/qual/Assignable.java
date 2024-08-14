42681685ae59e6c449d6cbe999004826e836d2e7
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2024-08-14 01:07:42 -0400

    Code clean

--- src/main/java/qual/Assignable.java
+++ src/main/java/qual/Assignable.java
@@ -12,7 +12,6 @@ import java.lang.annotation.Target;
  * This annotation is used to exclude the field from the abstract state and means the field can be reassigned after
  * initialization. It should only annotate on field, not class or method.
  */
-
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD})
