beb54431a073ced36857787fc0cf1823611af75a
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2024-11-14 17:04:32 -0800

    Add back missing inferable test cases

---
+++ Test.java
@@ -0,0 +1,13 @@
+import org.checkerframework.checker.pico.qual.*;
+//@Immutable
+//public class Test implements Test2 {
+//
+//}
+//
+//
+//interface Test2 {}
+
+@Immutable
+public class Test {
+    @Mutable Test(){}
+}
